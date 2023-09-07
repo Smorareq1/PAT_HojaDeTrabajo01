0eb286d1799b1f5caaa4d55239d93209e6b0a169
Daniel Mejia <rdmejiaa@gmail.com>
2023-09-07 07:04:59 -0600

    ejercicio 1 y 2 resueltos prueba 2Disable c-cpp workflow

--- HojaDeTrabajo01/Ejercicio03.cpp
+++ HojaDeTrabajo01/Ejercicio03.cpp
@@ -1,6 +1,14 @@
 #include "Ejercicio03.h"
+#include<math.h>
+#include<cstdlib>
+#include<cmath>
+
+using namespace std;
 
 bool Ejercicio03::isPowerOfTwo(int n)
-{
-    return false;
+{   
+    int nabs = abs(n);
+    log
+    
+    if(log_2nabs == floor(log_2nabs))
 }
--- HojaDeTrabajo01/HojaDeTrabajo01.cpp
+++ HojaDeTrabajo01/HojaDeTrabajo01.cpp
@@ -2,10 +2,11 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Sebastian Morales 1057123 !\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
